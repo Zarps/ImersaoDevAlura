58bb58101934fd90981ec1f6c1ea679a09026861
wesley rios <wes.alves.rios@gmail.com>
2022-03-10 12:25:39 -0300

    aula 4 imersão dev

---
+++ Projeto 4 Aluraflix/assets/css/js/script.js
@@ -0,0 +1,16 @@
+var listaFilmes = [
+    "https://s2.glbimg.com/fCNS_fXPhGRwlpnAaQLzvGYC1y0=/362x536/https://s2.glbimg.com/UPI9xlM9R9O41YRqSO7R3hTNk_s=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/q/V/Avo9ISQQO3grnARi3JCA/2020-914-a-chegada-poster.jpg",
+    "https://lumiere-a.akamaihd.net/v1/images/poster_semd_c4c0dfaa.png",
+    "https://m.media-amazon.com/images/I/61pPorlSduL._AC_SX522_.jpg",
+    "https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg",
+    "https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg",
+    "https://m.media-amazon.com/images/I/91hkDgKGheL._AC_SX425_.jpg"
+  ];
+  
+  
+  
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+    
+  }
+  
